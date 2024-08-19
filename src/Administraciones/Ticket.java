c01f7ddf48c87f88834a2878ca03cbc0cc1582f4
LChiza5 <lualvarezva@est.utn.ac.cr>
2024-08-19 15:03:38 -0600

    Clase Ticket
    Se creo la lista Ticket para que se pueda usar el hashset

--- src/Administraciones/Ticket.java
+++ src/Administraciones/Ticket.java
@@ -4,10 +4,51 @@
  */
 package Administraciones;
 
+import java.util.Objects;
+
 /**
  *
  * @author Student
  */
 class Ticket {
+    private String id;
+    private String descripcion; 
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Ticket(String id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        return id != null ? id.equals(ticket.id) : ticket.id == null;
+    }
+
+   @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "id=" + id + ", descripcion=" + descripcion + '}';
+    }
     
 }
